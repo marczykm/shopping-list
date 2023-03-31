e879013697c5eb3288f305f2e53d7d6789055338
Marcin Marczyk <marcin.marczyk@ocado.com>
2023-03-31 14:34:07 +0200

    11

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,18 +1,22 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Ingredient } from 'src/app/shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list.service';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
   styleUrls: ['./shopping-edit.component.css']
 })
-export class ShoppingEditComponent {
+export class ShoppingEditComponent implements OnInit {
 
-  @Output() newIngredientEvent = new EventEmitter<Ingredient>();
+  constructor(private shoppingListService: ShoppingListService){}
+
+  ngOnInit(): void {
+  }
 
   onAdd(name: HTMLInputElement, amount: HTMLInputElement){
     const newIngredient = new Ingredient(name.value, amount.valueAsNumber);
-    this.newIngredientEvent.emit(newIngredient);
+    this.shoppingListService.addIngredient(newIngredient);
   }
 
 }
