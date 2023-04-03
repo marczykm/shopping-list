e4f441bd8322de8135a5ae0d63b768edc779a4fb
Marcin Marczyk <marcin.marczyk@ocado.com>
2023-04-03 11:50:09 +0200

    12

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,13 +1,25 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { Ingredient } from 'src/app/shared/ingredient.model';
+import { ShoppingListService } from 'src/app/shopping-list/shopping-list.service';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-detail',
   templateUrl: './recipe-detail.component.html',
   styleUrls: ['./recipe-detail.component.css']
 })
-export class RecipeDetailComponent {
+export class RecipeDetailComponent implements OnInit {
 
   @Input() recipe: Recipe;
+
+  constructor(private recipeService: RecipeService) {}
+
+  ngOnInit(): void {
+  }
+
+  onAddToShoppingList() {
+    this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
+  }
 
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,31 +1,43 @@
-import { EventEmitter } from "@angular/core";
+import { EventEmitter, Injectable, OnInit } from "@angular/core";
 import { Recipe } from "./recipe.model";
 import { Ingredient } from "../shared/ingredient.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
 
-export class RecipeService {
+@Injectable()
+export class RecipeService implements OnInit {
 
-    recipeSelected = new EventEmitter<Recipe>();
+  constructor(private shoppingListService: ShoppingListService) { }
 
-    private recipes: Recipe[] = [
-        new Recipe(
-          'A Test Recipe 1', 
-          'This is simply a test 1', 
-          'https://upload.wikimedia.org/wikipedia/commons/thumb/5/57/990402-ians-recipe-01-IMG_4724.jpg/800px-990402-ians-recipe-01-IMG_4724.jpg',
-          [
-            new Ingredient('Meat', 1), new Ingredient('French Fries', 20)
-          ]
-          ),
-        new Recipe(
-          'A Test Recipe 2', 
-          'This is simply a test 2', 
-          'https://upload.wikimedia.org/wikipedia/commons/thumb/5/57/990402-ians-recipe-01-IMG_4724.jpg/800px-990402-ians-recipe-01-IMG_4724.jpg',
-          [
-            new Ingredient('Buns', 2), new Ingredient('Meat', 1)
-          ]
-          )  
-      ];
+  ngOnInit(): void {
+  }
 
-      getRecipes() {
-        return this.recipes.slice();
-      }
+  recipeSelected = new EventEmitter<Recipe>();
+
+  private recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe 1',
+      'This is simply a test 1',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/5/57/990402-ians-recipe-01-IMG_4724.jpg/800px-990402-ians-recipe-01-IMG_4724.jpg',
+      [
+        new Ingredient('Meat', 1), new Ingredient('French Fries', 20)
+      ]
+    ),
+    new Recipe(
+      'A Test Recipe 2',
+      'This is simply a test 2',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/5/57/990402-ians-recipe-01-IMG_4724.jpg/800px-990402-ians-recipe-01-IMG_4724.jpg',
+      [
+        new Ingredient('Buns', 2), new Ingredient('Meat', 1)
+      ]
+    )
+  ];
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.shoppingListService.addIngredients(ingredients);
+  }
+
 }
