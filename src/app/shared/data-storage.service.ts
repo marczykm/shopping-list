c0d5ed89a32c4a481dbd601b8cc9ca7e1db70f92
Marcin Marczyk <marcin.marczyk@ocado.com>
2023-05-29 17:31:53 +0200

    285

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { RecipeService } from "../recipes/recipe.service";
 import { Recipe } from "../recipes/recipe.model";
-import { map } from "rxjs/operators";
+import { map, tap } from "rxjs/operators";
 
 @Injectable({providedIn: 'root'})
 export class DataStorageService {
@@ -19,15 +19,16 @@ export class DataStorageService {
     }
 
     fetchRecipes() {
-        this.http
+        return this.http
             .get<Recipe[]>('https://shopping-list-8abec-default-rtdb.europe-west1.firebasedatabase.app/recipes.json')
             .pipe(map(recipes => {
                 return recipes.map(recipe => {
                     return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []}
                 });
-            }))
-            .subscribe(recipes => {
-                this.recipesService.setRecipes(recipes);
-            });
+            }),
+                tap(recipes => {
+                    this.recipesService.setRecipes(recipes);
+                })
+            );
     }
 }
