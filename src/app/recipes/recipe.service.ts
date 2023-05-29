125a578464571dfbe81211f2039e5de88c253511
Marcin Marczyk <marcin.marczyk@ocado.com>
2023-05-29 17:21:41 +0200

    284

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -13,24 +13,25 @@ export class RecipeService implements OnInit {
   ngOnInit(): void {
   }
 
-  private recipes: Recipe[] = [
-    new Recipe(
-      'A Test Recipe 1',
-      'This is simply a test 1 This is simply a test 1 This is simply a test 1 This is simply a test 1 This is simply a test 1 This is simply a test 1 This is simply a test 1 This is simply a test 1 ',
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/5/57/990402-ians-recipe-01-IMG_4724.jpg/800px-990402-ians-recipe-01-IMG_4724.jpg',
-      [
-        new Ingredient('Meat', 1), new Ingredient('French Fries', 20)
-      ]
-    ),
-    new Recipe(
-      'A Test Recipe 2',
-      'This is simply a test 2',
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/5/57/990402-ians-recipe-01-IMG_4724.jpg/800px-990402-ians-recipe-01-IMG_4724.jpg',
-      [
-        new Ingredient('Buns', 2), new Ingredient('Meat', 1)
-      ]
-    )
-  ];
+  // private recipes: Recipe[] = [
+  //   new Recipe(
+  //     'A Test Recipe 1',
+  //     'This is simply a test 1 This is simply a test 1 This is simply a test 1 This is simply a test 1 This is simply a test 1 This is simply a test 1 This is simply a test 1 This is simply a test 1 ',
+  //     'https://upload.wikimedia.org/wikipedia/commons/thumb/5/57/990402-ians-recipe-01-IMG_4724.jpg/800px-990402-ians-recipe-01-IMG_4724.jpg',
+  //     [
+  //       new Ingredient('Meat', 1), new Ingredient('French Fries', 20)
+  //     ]
+  //   ),
+  //   new Recipe(
+  //     'A Test Recipe 2',
+  //     'This is simply a test 2',
+  //     'https://upload.wikimedia.org/wikipedia/commons/thumb/5/57/990402-ians-recipe-01-IMG_4724.jpg/800px-990402-ians-recipe-01-IMG_4724.jpg',
+  //     [
+  //       new Ingredient('Buns', 2), new Ingredient('Meat', 1)
+  //     ]
+  //   )
+  // ];
+  private recipes: Recipe[] = [];
 
   setRecipes(recipes: Recipe[]){
     this.recipes = recipes;
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { RecipeService } from "../recipes/recipe.service";
 import { Recipe } from "../recipes/recipe.model";
+import { map } from "rxjs/operators";
 
 @Injectable({providedIn: 'root'})
 export class DataStorageService {
@@ -9,17 +10,24 @@ export class DataStorageService {
 
     storeRecipes() {
         const recipes = this.recipesService.getRecipes();
-        this.http.put('https://shopping-list-8abec-default-rtdb.europe-west1.firebasedatabase.app/recipes.json',
-            recipes
-        ).subscribe(response => {
-            console.log(response);
-        })
+        this.http
+            .put('https://shopping-list-8abec-default-rtdb.europe-west1.firebasedatabase.app/recipes.json',
+                recipes
+            ).subscribe(response => {
+                console.log(response);
+            })
     }
 
     fetchRecipes() {
-        this.http.get<Recipe[]>('https://shopping-list-8abec-default-rtdb.europe-west1.firebasedatabase.app/recipes.json')
-        .subscribe(recipes => {
-            this.recipesService.setRecipes(recipes);
-        });
+        this.http
+            .get<Recipe[]>('https://shopping-list-8abec-default-rtdb.europe-west1.firebasedatabase.app/recipes.json')
+            .pipe(map(recipes => {
+                return recipes.map(recipe => {
+                    return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []}
+                });
+            }))
+            .subscribe(recipes => {
+                this.recipesService.setRecipes(recipes);
+            });
     }
 }
