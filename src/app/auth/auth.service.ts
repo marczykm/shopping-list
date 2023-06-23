42e62591a100e4712fdb75c7aa2b65154ad58dd1
Marcin Marczyk <marcin.marczyk@ocado.com>
2023-06-23 21:01:17 +0200

    302

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { catchError, tap } from "rxjs/operators";
-import { Subject, throwError } from "rxjs";
+import { BehaviorSubject, throwError } from "rxjs";
 import { User } from "./user.model";
 
 export interface AuthResponseData {
@@ -16,11 +16,9 @@ export interface AuthResponseData {
 @Injectable({providedIn: 'root'})
 export class AuthService {
 
-    user = new Subject<User>();
+    user = new BehaviorSubject<User>(null);
 
-    constructor(private http: HttpClient) {
-
-    }
+    constructor(private http: HttpClient) {}
 
     signup(email: string, password: string) {
         return this.http.post<AuthResponseData>(
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,12 +1,17 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpParams } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { RecipeService } from "../recipes/recipe.service";
 import { Recipe } from "../recipes/recipe.model";
-import { map, tap } from "rxjs/operators";
+import { exhaustMap, map, take, tap } from "rxjs/operators";
+import { AuthService } from "../auth/auth.service";
 
 @Injectable({providedIn: 'root'})
 export class DataStorageService {
-    constructor(private http: HttpClient, private recipesService: RecipeService){}
+    constructor(
+        private http: HttpClient, 
+        private recipesService: RecipeService,
+        private authServie: AuthService
+        ){}
 
     storeRecipes() {
         const recipes = this.recipesService.getRecipes();
@@ -19,16 +24,19 @@ export class DataStorageService {
     }
 
     fetchRecipes() {
-        return this.http
-            .get<Recipe[]>('https://shopping-list-8abec-default-rtdb.europe-west1.firebasedatabase.app/recipes.json')
-            .pipe(map(recipes => {
-                return recipes.map(recipe => {
-                    return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []}
-                });
-            }),
-                tap(recipes => {
-                    this.recipesService.setRecipes(recipes);
-                })
+        return this.authServie.user.pipe(take(1), exhaustMap(user => {
+            return this.http.get<Recipe[]>(
+                'https://shopping-list-8abec-default-rtdb.europe-west1.firebasedatabase.app/recipes.json',
+                {params: new HttpParams().set('auth', user.token)}
             );
+        }), 
+        map(recipes => {
+            return recipes.map(recipe => {
+                return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []}
+            });
+        }),
+        tap(recipes => {
+            this.recipesService.setRecipes(recipes);
+        }));
     }
 }
