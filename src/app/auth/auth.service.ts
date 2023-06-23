b2575e1ccb5cb8b4c70dbdc37d47666e534e203b
Marcin Marczyk <marcin.marczyk@ocado.com>
2023-06-23 15:42:33 +0200

    298

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -3,12 +3,13 @@ import { Injectable } from "@angular/core";
 import { catchError } from "rxjs/operators";
 import { throwError } from "rxjs";
 
-interface AuthResponseData {
+export interface AuthResponseData {
     idToken: string;
     email: string;
     refreshToken: string;
     expiresIn: string;
     localId: string;
+    registered?: boolean;
 }
 
 @Injectable({providedIn: 'root'})
@@ -39,9 +40,26 @@ export class AuthService {
                     errorMessage = 'Password sign-in is disabled for this project.';
                 case 'TOO_MANY_ATTEMPTS_TRY_LATER':
                     errorMessage = 'We have blocked all requests from this device due to unusual activity. Try again later.';
+                case 'EMAIL_NOT_FOUND':
+                    errorMessage = 'There is no user record corresponding to this identifier. The user may have been deleted.';
+                case 'INVALID_PASSWORD':
+                    errorMessage = 'The password is invalid or the user does not have a password.';
+                case 'USER_DISABLED':
+                    errorMessage = 'The user account has been disabled by an administrator.';
             }
             return throwError(errorMessage);
         }));
+    }
+
+    login(email: string, password: string){
+        return this.http.post<AuthResponseData>(
+            'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDy82Tn8uF0xEmD_JTzjXruOKgLVMXSfH4',
+            {
+                email: email,
+                password: password,
+                returnSecureToken: true
+            }
+        )
     }
 
 }
