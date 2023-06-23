a6f4877a624240a5ebcc0405c1c24a8d6ed51528
Marcin Marczyk <marcin.marczyk@ocado.com>
2023-06-23 15:55:06 +0200

    299

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { catchError } from "rxjs/operators";
 import { throwError } from "rxjs";
@@ -27,28 +27,7 @@ export class AuthService {
                 password: password,
                 returnSecureToken: true
             }
-        ).pipe(catchError(errorRes => {
-            let errorMessage = 'Unknown error occured';
-
-            if (!errorRes.error || !errorRes.error.error){
-                return throwError(errorMessage);
-            }
-            switch(errorRes.error.error.message){
-                case 'EMAIL_EXISTS':
-                    errorMessage = 'The email address is already in use by another account.';
-                case 'OPERATION_NOT_ALLOWED':
-                    errorMessage = 'Password sign-in is disabled for this project.';
-                case 'TOO_MANY_ATTEMPTS_TRY_LATER':
-                    errorMessage = 'We have blocked all requests from this device due to unusual activity. Try again later.';
-                case 'EMAIL_NOT_FOUND':
-                    errorMessage = 'There is no user record corresponding to this identifier. The user may have been deleted.';
-                case 'INVALID_PASSWORD':
-                    errorMessage = 'The password is invalid or the user does not have a password.';
-                case 'USER_DISABLED':
-                    errorMessage = 'The user account has been disabled by an administrator.';
-            }
-            return throwError(errorMessage);
-        }));
+        ).pipe(catchError(this.handleError));
     }
 
     login(email: string, password: string){
@@ -59,7 +38,36 @@ export class AuthService {
                 password: password,
                 returnSecureToken: true
             }
-        )
+        ).pipe(catchError(this.handleError));
+    }
+
+    private handleError(errorRes: HttpErrorResponse){
+        let errorMessage = 'Unknown error occured';
+
+        if (!errorRes.error || !errorRes.error.error){
+            return throwError(errorMessage);
+        }
+        switch(errorRes.error.error.message){
+            case 'EMAIL_EXISTS':
+                errorMessage = 'The email address is already in use by another account.';
+                break;
+            case 'OPERATION_NOT_ALLOWED':
+                errorMessage = 'Password sign-in is disabled for this project.';
+                break;
+            case 'TOO_MANY_ATTEMPTS_TRY_LATER':
+                errorMessage = 'We have blocked all requests from this device due to unusual activity. Try again later.';
+                break;
+            case 'EMAIL_NOT_FOUND':
+                errorMessage = 'There is no user record corresponding to this identifier. The user may have been deleted.';
+                break;
+            case 'INVALID_PASSWORD':
+                errorMessage = 'The password is invalid or the user does not have a password.';
+                break;
+            case 'USER_DISABLED':
+                errorMessage = 'The user account has been disabled by an administrator.';
+                break;
+        }
+        return throwError(errorMessage);
     }
 
 }
